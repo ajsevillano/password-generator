ac23674e1589b23ab37074ab7863ea8cc9189c84
Sevi <correosevi@gmail.com>
2021-03-26 13:38:46 +0000

    Added react-icons package

--- src/App.js
+++ src/App.js
@@ -4,12 +4,15 @@ import Layout from './components/Layout/Index';
 import Input from './components/Input/Index';
 import Button from './components/Button/index';
 
+import { FcLock } from 'react-icons/fc';
+
 function App() {
   return (
     <Layout>
       <h1>Password Generator</h1>
       <p> Generate a secure password to use on your daily basics!</p>
       <div className="input-container">
+        <FcLock />
         <Input />
         <Button />
       </div>
--- src/components/Input/Index.js
+++ src/components/Input/Index.js
@@ -1,5 +1,5 @@
 const Input = () => {
-  return <input type="text" disabled />;
+  return <input type="text" disabled placeholder="Dasdlñk4asdfazx@|" />;
 };
 
 export default Input;
