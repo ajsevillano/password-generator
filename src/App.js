bcbc9eb74cc940fa2aab00b02d71f1590d069195
Sevi <correosevi@gmail.com>
2021-03-26 11:49:20 +0000

    Create Input component

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import './App.css';
 
 import Layout from './components/Layout/Index';
+import Input from './components/Input/Index';
 
 function App() {
   return (
     <Layout>
       <h1>Password Generator</h1>
       <div className="input-container">
-        <input type="text" />
+        <Input />
         <button>Generate</button>
       </div>
     </Layout>
