23073328847f9de405171dfa36172acaf78fe07d
Sevi <correosevi@gmail.com>
2021-04-03 12:59:13 +0100

    Remove side bar

--- src/components/Layout/Index.js
+++ src/components/Layout/Index.js
@@ -3,7 +3,6 @@ import React from 'react';
 const Index = ({ children }) => {
   return (
     <div className="container">
-      <div className="side"></div>
       <div className="main-contain">{children}</div>
     </div>
   );
