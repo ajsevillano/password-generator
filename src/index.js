974126fbf9a873d83e85c37767c3a4e08045a733
Sevi <correosevi@gmail.com>
2022-06-18 14:00:04 +0100

    Update to latest version of react

--- src/index.js
+++ src/index.js
@@ -1,14 +1,15 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom/client';
 import './Globals.css';
 import App from './components/App';
 import { GlobalContextProvider } from './context/GlobalContext';
 
-ReactDOM.render(
+const root = ReactDOM.createRoot(document.getElementById('root'));
+
+root.render(
   <React.StrictMode>
     <GlobalContextProvider>
       <App />
     </GlobalContextProvider>
-  </React.StrictMode>,
-  document.getElementById('root')
+  </React.StrictMode>
 );
