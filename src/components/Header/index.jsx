16726d931d8e3366242067ea02334906fc50b701
Sevi <correosevi@gmail.com>
2022-06-18 13:26:31 +0100

    Update react lottie package

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,14 +1,15 @@
+//Components
 import Title from '../Title';
 
 //Lottie animation helper and json import
-import LottieAnimation from '../../utils/Lottie';
-import * as lock from '../../assets/lock.json';
+import Lottie from 'lottie-react';
+import lock from '../../assets/lock.json';
 
 const Header = () => {
   return (
     <div className="header" data-testid="header">
       <div className="lottie-animation">
-        <LottieAnimation animationName={lock} />
+        <Lottie animationData={lock} />
       </div>
 
       <Title />
