0f9ac1739823b92a866d7f57cf850a9147636f76
Sevi <correosevi@gmail.com>
2022-06-18 16:07:45 +0100

    create title tests

---
+++ src/components/Title/Title.test.js
@@ -0,0 +1,24 @@
+import { render, screen } from '@testing-library/react';
+import '@testing-library/jest-dom';
+import Title from './';
+// import { GlobalContextProvider } from '../../context/GlobalContext';
+
+it('renders title component to the screen', () => {
+  render(<Title />);
+  const mainContainer = screen.getByTestId('title');
+  expect(mainContainer).toBeInTheDocument();
+});
+
+it('check the title is in the component', () => {
+  render(<Title />);
+  const title = screen.getByText(/Do you need a secure password?/i);
+  expect(title).toBeInTheDocument();
+});
+
+it('check the subtitle is in the component', () => {
+  render(<Title />);
+  const subTitle = screen.getByText(
+    /Generate a secure password to use on a daily basics!/i
+  );
+  expect(subTitle).toBeInTheDocument();
+});
--- src/components/Title/index.jsx
+++ src/components/Title/index.jsx
@@ -1,6 +1,6 @@
 const Title = () => {
   return (
-    <div className="title-container">
+    <div data-testid="title" className="title-container">
       <h1>Do you need a secure password?</h1>
       <p> Generate a secure password to use on a daily basics!</p>
     </div>
