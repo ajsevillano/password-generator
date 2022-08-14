bb0013b5b4572dfb2a42fcdb74ed87797b64a575
Sevi <correosevi@gmail.com>
2022-08-14 20:14:22 +0100

    convert checkbox test to typescript

--- src/components/Checkbox/Checkbox.test.tsx
+++ src/components/Checkbox/Checkbox.test.tsx
@@ -1,6 +1,7 @@
+import React from 'react';
 import { render, screen } from '@testing-library/react';
 import '@testing-library/jest-dom';
-import CheckBox from './';
+import CheckBox from '.';
 import { GlobalContextProvider } from '../../context/GlobalContext';
 
 it('renders the checkbox to the screen', () => {
