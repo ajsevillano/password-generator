8317c385bf908846e7426a3c27b8cac302ea6223
Sevi <correosevi@gmail.com>
2022-08-28 19:38:04 +0100

    Fix types on Input component

--- src/components/Input/index.tsx
+++ src/components/Input/index.tsx
@@ -4,18 +4,16 @@ import { useContext } from 'react';
 import React from 'react';
 
 //Interfaces
-interface Contexts {
-  [loadingState: string]: {};
-}
-
-//TO FIX
-interface Password {
-  [loadingState: string]: string;
+interface ContextValuesObject {
+  passwordState: {
+    password: string;
+    setPassword: React.Dispatch<React.SetStateAction<string>>;
+  };
 }
 
 const Input = () => {
-  const { passwordState }: Contexts = useContext(GlobalContext);
-  const { password }: Password = passwordState;
+  const { passwordState }: ContextValuesObject = useContext(GlobalContext);
+  const { password }: ContextValuesObject['passwordState'] = passwordState;
 
   return (
     <input data-testid="input" type="text" defaultValue={password} readOnly />
