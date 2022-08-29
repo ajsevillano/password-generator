232852f3a5e9e17c91a10f6c7ab79a36e48519a7
Sevi <correosevi@gmail.com>
2022-08-29 20:13:43 +0100

    Declare root element as HTMLElement to fix typeerror instead or !

--- src/index.tsx
+++ src/index.tsx
@@ -4,8 +4,8 @@ import './Globals.css';
 import App from './components/App';
 import { GlobalContextProvider } from './context/GlobalContext';
 
-const container = document.getElementById('root');
-const root = createRoot(container!);
+const container = document.getElementById('root') as HTMLElement;
+const root = createRoot(container);
 
 root.render(
   <React.StrictMode>
